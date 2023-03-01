94cf2e74900ecbe4989f95f48f10549d0d4c33fd
Rund Tawfiq <107937921+rund0wn@users.noreply.github.com>
2023-03-01 16:50:42 +0300

    Update 00_QC.sh

--- 00_QC.sh
+++ 00_QC.sh
@@ -1,13 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=QC
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --time=05:00:00
+#SBATCH --mem=20GB
+
 # Create directories and load required modules
 mkdir QC
 module load fastqc
 module load fastp
 
 # Run fastQC on read files (quality control metrics/graphs)
-fastqc ${1}_1.fastq.gz -o QC
-fastqc ${1}_2.fastq.gz -o QC
+fastqc ${1}_1.fastq.gz -o QC -t 6
+fastqc ${1}_2.fastq.gz -o QC -t 6
 
 # Run fastp on read files (trimming)
 fastp -i ${1}_1.fastq.gz -I ${1}_2.fastq.gz \
 -o ${1}_trimmed_R1.fastq.gz -O ${1}_trimmed_R2.fastq.gz \
--q 30
+-q 30 -w 6
